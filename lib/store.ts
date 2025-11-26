61b11568b6fd4e0ba38ca1114ed46d618ccff12b
Faisal Altaf <altaffaisal532@gmail.com>
2025-11-26 16:30:47 +0500

    Adding REDUX Toolkit

---
+++ lib/store.ts
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+// We will add reducers here later
+export const makeStore = () => {
+  return configureStore({
+    reducer: {},
+  });
+};
+
+// Infer the type of makeStore
+export type AppStore = ReturnType<typeof makeStore>;
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<AppStore['getState']>;
+export type AppDispatch = AppStore['dispatch'];
